a05ed5579bbcc62808c2e5732f9d728c22e96344
jell <infinite.ft@gmail.com>
2018-06-20 22:27:22 +0800

    add code to configure bucket_url

--- configer.py
+++ configer.py
@@ -21,15 +21,18 @@ def run():
     simple_request('backend', '请输入图片储存服务(qiniu)')
     if config['backend'].strip() == 'qiniu':
         simple_request('bucket', '请输入bucket name')
+        simple_request('bucket_url', '请输入bucket url')
         simple_request('access_key', '请输入access key')
         simple_request('secret_key', '请输入secret key')
         config['qiniu'] = {}
         config['qiniu']['bucket'] = config['bucket']
         config['qiniu']['access_key'] = config['access_key']
         config['qiniu']['secret_key'] = config['secret_key']
+        config['qiniu']['bucket_url'] = config['bucket_url']
         del config['bucket']
         del config['access_key']
         del config['secret_key']
+        del config['bucket_url']
     user_home = os.path.expanduser('~')
     config_file = os.path.join(user_home, '.mdiup_config')
     with open(config_file,'w') as f:
