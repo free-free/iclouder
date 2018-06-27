bd69b64bc198f9e6fddc7874082ade5f4f577a92
jell <infinite.ft@gmail.com>
2018-06-27 16:42:23 +0800

    Remove shell_echo function and replace it with color_print function that makes it to support all platforms. Adding color_input funtion, it is able to prompt colored text

--- iclouder/utils.py
+++ iclouder/utils.py
@@ -1,9 +1,17 @@
 # -*- coding:utf-8 -*-
 
 
+import sys
 import subprocess
 import shlex
 import datetime
+import functools
+from colorama import init as colorama_init
+from termcolor import colored, cprint
+
+
+
+COLORAMA_INITIATION_FLG = False
 
 
 def execute_shell_cmd(cmd, timeout=60, bufsize=4096):
@@ -21,17 +29,35 @@ def execute_shell_cmd(cmd, timeout=60, bufsize=4096):
     return sub.returncode
 
 
-def shell_echo(echo_string,color):
-    COLOR_CODE = {
-        'black': '0;30m',
-        'red': '0;31m',
-        'green': '0;32m',
-        'brown': '0:33m',
-        'blue': '0;34m'
-    }
-    cmd = 'echo -e "\e[{color}{string}\e[m"'.format(\
-            color=COLOR_CODE.get(color.lower(), '0;30m'),\
-                string=echo_string)
-    return execute_shell_cmd(cmd)
+def _chk_platform(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        """
+            Cause the module `colorama` is required to call 
+            `init` function when it's running windows, the 
+            purpose of code here is to check platform and so as to 
+            avoid repeatedly calling `init` function.
+        """
+        if sys.platform == 'win32':
+            global COLORAMA_INITATION_FLG
+            if not COLORAMA_INITIATION_FLG:
+                colorama_init()
+                COLORAMA_INITIATION_FLG = True
+        func(*args, **kwargs)
+    return _wrapper
+        
+
+@_chk_platform
+def color_input(prompt, color, highlight_color=None):
+    if isinstance(highlight_color, str):
+        highlight_color = 'on_' + highlight_color 
+    input(colored(prompt, color, highlight_color))
+
+
+@_chk_platform
+def color_print(text,color, highlight_color=None):
+    if isinstance(highlight_color, str):
+        highlight_color = 'on_' + highlight_color 
+    return cprint(text, color, highlight_color)
 
 
